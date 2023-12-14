012572bf1a446f6d0bc74a74fd8e86e9b180aa08
Anthony Velez <josuevelez@outlook.es>
2023-12-14 11:58:19 +0100

    plus

---
+++ controle-2.php
@@ -0,0 +1,41 @@
+<?php
+echo "quel mois somme-nous";
+$month=fgets(STDIN);
+switch ($month){
+    case "janvier":
+        echo "c'est le mois 1";
+        break;
+    case "février":
+        echo "le mois 2";
+        break;
+    case "mars":
+        echo "le mois 3";
+        break;
+    case "avril":
+        echo "le mois 4";
+        break;
+    case "mai":
+        echo "le mois 5";
+        break;
+    case "juin":
+        echo "le mois 6";
+        break;
+    case "juillet":
+        echo "le moi 7";
+        break;
+    case "aout":
+        echo "le moi 8";
+        break;
+    case "septembre":
+        echo "le moi 9 ";
+        break;
+    case "octobre":
+        echo "moi 10";
+        break;
+    case "novembre":
+        echo "le moi 11";
+        break;
+    case "décembre":
+        echo "le moi 12";
+        break;
+}
--- controle-3.php
+++ controle-3.php
@@ -7,17 +7,19 @@ if ($number >= 1){
 }
 elseif ($number == 0){
     do{
-        echo "tapez 1 célibataire 2 par choix ou 3 peut pas";
+        echo "tapez 1 célibataire 2 par choix ou 3 peut pas: ";
         $number2=fgets(STDIN);
         if ($number2 > 3 || $number2 < 1){
             echo "erreur de commande";
         }
     }
-    while($number2 = 1);
+    while($number2 != 1);
         echo "ok";
-
-    while ($number2 = 2){
-        echo "ok";
-    }
+}
+elseif ($number2 != 2){
+    echo "ok";
+}
+else {
+    echo "ok";
 }
 ?>
