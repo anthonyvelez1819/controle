dd82f11acdd54e34f73a6cd75694137abcd82c4e
Anthony Velez <josuevelez@outlook.es>
2023-12-14 10:50:40 +0100

    ajout exercice

---
+++ controle-3.php
@@ -0,0 +1,23 @@
+<?php
+echo "combien d'enfant avez-vous? ";
+$number=fgets(STDIN);
+$number=trim($number);
+if ($number >= 1){
+    echo "félicitation";
+}
+elseif ($number == 0){
+    do{
+        echo "tapez 1 célibataire 2 par choix ou 3 peut pas";
+        $number2=fgets(STDIN);
+        if ($number2 > 3 || $number2 < 1){
+            echo "erreur de commande";
+        }
+    }
+    while($number2 = 1);
+        echo "ok";
+
+    while ($number2 = 2){
+        echo "ok";
+    }
+}
+?>
